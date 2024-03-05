66e2bbf726e76c15621e65b68e0aab072482e8f1
Chuck Galen <162306837+frischomerci@users.noreply.github.com>
2024-03-05 12:20:37 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+  if(keyword == "v") {
+            Float extra{1.0f};
+            const Vector3 data = extractFloatData<3>(contents, &extra);
+            if(!Math::TypeTraits<Float>::equals(extra, 1.0f)) {
+                Error() << "Trade::ObjImporter::mesh(): homogeneous coordinates are not supported";
+                return Containers::NullOpt;
+            }
+
+
+    } else if(keyword == "vt") {
+            Float extra{0.0f};
+            const Vector2 data = extractFloatData<2>(contents, &extra);
+            if(!Math::TypeTraits<Float>::equals(extra, 0.0f)) {
+                Error() << "Trade::ObjImporter::mesh(): 3D texture coordinates are not supported";
+                return Containers::NullOpt;
+            }
+
+            arrayAppend(textureCoordinates, data);
+
+    } else if(keyword == "p" || keyword == "l" || keyword == "f") {
+            const std::vector<std::string> indexTuples = Utility::String::splitWithoutEmptyParts(contents, ' ');
